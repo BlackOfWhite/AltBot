65794bfd6d342c80866a6c1b1eb48e36fcde6e63
Piotr Niewinski <pniewinski@logopak.de>
2018-02-21 17:10:15 +0100

    Started implementing progress bar.

--- src/main/java/org/ui/views/list/orders/OrderListCellRenderer.java
+++ src/main/java/org/ui/views/list/orders/OrderListCellRenderer.java
@@ -1,5 +1,12 @@
 package org.ui.views.list.orders;
 
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.Tooltip;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -24,8 +31,35 @@ public class OrderListCellRenderer extends JPanel implements javax.swing.ListCel
                 (isSelected ? Color.white : HIGHLIGHT);
         JLabel jLabel = new JLabel(listElementOrder.getCoinName());
         JLabel jLabel2 = new JLabel(listElementOrder.getOrderType());
-        row.add(jLabel);
-        row.add(jLabel2);
+//        row.add(jLabel, BorderLayout.LINE_START);
+//        row.add(jLabel2, BorderLayout.LINE_END);
+        // Progress bar
+        final JFXPanel jfxPanel = new JFXPanel();
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                initFX(jfxPanel);
+            }
+        });
+        jfxPanel.setBackground(Color.lightGray);
+        jfxPanel.setSize(100,100);
+        row.add(jfxPanel, CENTER_ALIGNMENT);
         return row;
+    }
+
+    private void initFX(JFXPanel jfxPanel) {
+        Scene scene = createScene();
+        jfxPanel.setScene(scene);
+        jfxPanel.setSize(100,100);
+    }
+
+    private Scene createScene() {
+        Group root = new Group();
+        Scene scene = new Scene(root);
+        ProgressBar progressBar = new ProgressBar();
+        progressBar.setProgress(69);
+        progressBar.setMinSize(100,100);
+        root.getChildren().add(progressBar);
+        return (scene);
     }
 }
